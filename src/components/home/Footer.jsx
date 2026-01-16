630592ac86fef4d619b647e139dfd8d07a564cf7
Anurag Vaidhya <pinakianurag@mac.lan>
2026-01-16 14:07:58 +1100

    added google analytics

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -137,7 +137,7 @@ const Footer = () => {
               
               <div className="footer-copyright">
                 <span>© {new Date().getFullYear()} Anurag Vaidhya. All rights reserved.</span>
-                <span className="version-badge">v2.1.4</span>
+                <span className="version-badge">v2.2.0</span>
               </div>
             </div>
           </div>
